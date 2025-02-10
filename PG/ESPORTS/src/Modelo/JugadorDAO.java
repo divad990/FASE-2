726365526ae914684be1c0cd772bc79d293fa658
David <david.gonzalez@ikasle.egibide.org>
2025-02-10 10:44:28 +0100

    Creación DAO jugador y HashMap

--- PG/ESPORTS/src/Modelo/JugadorDAO.java
+++ PG/ESPORTS/src/Modelo/JugadorDAO.java
@@ -1,4 +1,14 @@
 package Modelo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JugadorDAO {
+
+    public static final Map<String, Jugador> jugadores = new HashMap<>();
+
+    public static void crearJugador(Jugador j){
+        jugadores.put(j.getNickname(),j);
+    }
+
 }
