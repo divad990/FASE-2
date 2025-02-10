d79ef44bb6592ff9ae339496b3861cf9360f555d
ikerh <ikerhg37@gmail.com>
2025-02-10 09:40:44 +0100

    Clase competicion con atributos,get and set y constructores

--- PG/ESPORTS/src/Modelo/Competicion.java
+++ PG/ESPORTS/src/Modelo/Competicion.java
@@ -3,4 +3,39 @@ package Modelo;
 public class Competicion {
     private int idCompeticion;
     private boolean inscripcion;
+    private Jornada jornada;
+
+    public Competicion() {
+
+    }
+
+    public Competicion(int idCompeticion, boolean inscripcion, Jornada jornada) {
+        this.idCompeticion = idCompeticion;
+        this.inscripcion = inscripcion;
+        this.jornada = jornada;
+    }
+
+    public int getIdCompeticion() {
+        return idCompeticion;
+    }
+
+    public void setIdCompeticion(int idCompeticion) {
+        this.idCompeticion = idCompeticion;
+    }
+
+    public Jornada getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(Jornada jornada) {
+        this.jornada = jornada;
+    }
+
+    public boolean isInscripcion() {
+        return inscripcion;
+    }
+
+    public void setInscripcion(boolean inscripcion) {
+        this.inscripcion = inscripcion;
+    }
 }
