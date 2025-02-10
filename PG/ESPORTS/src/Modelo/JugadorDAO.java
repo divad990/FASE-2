c91f23ba553bf0877518bef8e03d8d02dcb5f1c7
David <david.gonzalez@ikasle.egibide.org>
2025-02-10 10:56:25 +0100

    DAO completo jugadores

--- PG/ESPORTS/src/Modelo/JugadorDAO.java
+++ PG/ESPORTS/src/Modelo/JugadorDAO.java
@@ -10,5 +10,24 @@ public class JugadorDAO {
     public static void crearJugador(Jugador j){
         jugadores.put(j.getNickname(),j);
     }
+    public static void eliminarJugador(String nickname){
+        jugadores.remove(nickname);
+    }
+    public static void mostrarJugadores(StringBuilder vista){
+        vista.delete(0, vista.length());
+        for (Map.Entry<String, Jugador> entry : jugadores.entrySet()) {
+            vista.append("Nombre: ").append(entry.getValue().getNombre())
+                    .append("\t Apellidos: ").append(entry.getValue().getApellido())
+                    .append("\t Nacionalidad: ").append(entry.getValue().getNacionalidad())
+                    .append("\t Fecha de Nacimiento: ").append(entry.getValue().getFechaNacimiento())
+                    .append("\t Nickname: ").append(entry.getValue().getNickname())
+                    .append("\t Rol: ").append(entry.getValue().getRol())
+                    .append("\t Sueldo: ").append(entry.getValue().getSueldo())
+                    .append("\n");
+        }
+    }
+    public static void mostrarJugadorActual(StringBuilder vista2){
+        mostrarJugadores(vista2);
+    }
 
 }
