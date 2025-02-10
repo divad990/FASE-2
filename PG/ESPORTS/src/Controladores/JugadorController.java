dfc1ed8cb5eee4dde4d3e758e1b1c3a34f88b89a
David <david.gonzalez@ikasle.egibide.org>
2025-02-10 10:44:00 +0100

    Funciones complementarias y crear jugador

--- PG/ESPORTS/src/Controladores/JugadorController.java
+++ PG/ESPORTS/src/Controladores/JugadorController.java
@@ -1,8 +1,12 @@
 package Controladores;
 
 import Modelo.Jugador;
+import Modelo.JugadorDAO;
+import utils.Utilidades;
 
 import javax.swing.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class JugadorController {
 
@@ -12,7 +16,7 @@ public class JugadorController {
             do {
                 Jugador j = solicitarDatos();
                 JugadorDAO.crearJugador(j);
-                System.out.println("Jugador con nombre " + j.getNombre() + " creado correctamente");
+                System.out.println("Jugador con nickname " + j.getNickname() + " creado correctamente");
                 //fin creación -- preguntar si se quiere crear otro jugador
                 String respuesta = JOptionPane.showInputDialog("¿Quieres crear otro jugador? (s/n)");
                 if (respuesta.equalsIgnoreCase("n")) {
@@ -26,14 +30,21 @@ public class JugadorController {
 
     //INICIO FUNCIONES COMPLEMENTARIAS
     public static Jugador solicitarDatos(){
-        String nombre = Utilidades.solicitarDato("Nombre", "Teclea el nombre del cliente","^[A-Z][a-z]+([ -][A-Z][a-z]+)*$");
-        String apellidos = Utilidades.solicitarDato("Apellido","Teclea la el apellido del cliente","^[A-Z][a-z]+([ -][A-Z][a-z]+)*$");
-        String dni = Utilidades.solicitarDato("DNI","Teclea el DNI del cliente","^[0-9]{8}[A-Z]$");
-        String direccion = Utilidades.solicitarDato("Direccion","Teclea la direccion del cliente","^[A-Z][a-z]+([ -][A-Z][a-z]+)*$");
-        int telefono = Integer.parseInt(Utilidades.solicitarDato("Telefono","Teclea el telefono del cliente","^[0-9]{9}$"));
-        String email = Utilidades.solicitarDato("Email","Teclea el email del cliente","^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$");
+        String nombre = Utilidades.solicitarDato("Nombre", "Teclea el nombre del cliente", "^[A-Z][a-z]+([ -][A-Z][a-z]+)*$");
+        String apellidos = Utilidades.solicitarDato("Apellido", "Teclea el apellido del cliente", "^[A-Z][a-z]+([ -][A-Z][a-z]+)*$");
+        String nacionalidad = Utilidades.solicitarDato("Nacionalidad", "Teclea la nacionalidad del cliente", "^[A-Z][a-z]+([ -][A-Z][a-z]+)*$");
+        LocalDate fechaNacimiento = convertirLocalDate((Utilidades.solicitarDato("Fecha de Nacimiento", "Teclea la fecha de nacimiento del cliente (yyyy-MM-dd)","^[0-9]{2}/[0-9]{2}/[0-9]{4}$")));
+        String nickname = Utilidades.solicitarDato("Nickname", "Teclea el nickname del cliente", "^[A-Za-z0-9_]+$");
+        String rol = Utilidades.solicitarDato("Rol", "Teclea el rol del cliente", "^[A-Za-z]+$");
+        int sueldo = Integer.parseInt(Utilidades.solicitarDato("Sueldo", "Teclea el sueldo del cliente", "^[0-9]+$"));
 
-        return new Jugador(nombre,apellidos,dni,direccion,telefono,email,null);
+        return new Jugador(nombre, apellidos, nacionalidad, fechaNacimiento, nickname, rol, sueldo, null);
+    }
+    public static LocalDate convertirLocalDate(String fecha)
+    {
+        DateTimeFormatter formato;
+        formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(fecha, formato);
     }
 
 }
