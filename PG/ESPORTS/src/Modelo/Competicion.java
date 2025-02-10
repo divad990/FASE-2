1fff5e97a5ddfb9cb6cf31e55a5bb1a4bde1cd22
ikerh <ikerhg37@gmail.com>
2025-02-10 10:18:14 +0100

    Clase enfrentamiento,jornada con atributos,get and set y constructores

--- PG/ESPORTS/src/Modelo/Competicion.java
+++ PG/ESPORTS/src/Modelo/Competicion.java
@@ -1,18 +1,20 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Competicion {
     private int idCompeticion;
     private boolean inscripcion;
-    private Jornada jornada;
+    private ArrayList<Jornada> listaJornadas ;
 
     public Competicion() {
 
     }
 
-    public Competicion(int idCompeticion, boolean inscripcion, Jornada jornada) {
+    public Competicion(int idCompeticion, boolean inscripcion, ArrayList listaJornadas) {
         this.idCompeticion = idCompeticion;
         this.inscripcion = inscripcion;
-        this.jornada = jornada;
+        this.listaJornadas = listaJornadas;
     }
 
     public int getIdCompeticion() {
@@ -23,12 +25,12 @@ public class Competicion {
         this.idCompeticion = idCompeticion;
     }
 
-    public Jornada getJornada() {
-        return jornada;
+    public ArrayList<Jornada> getListaJornadas() {
+        return listaJornadas;
     }
 
-    public void setJornada(Jornada jornada) {
-        this.jornada = jornada;
+    public void setListaJornadas(ArrayList<Jornada> listaJornadas) {
+        this.listaJornadas = listaJornadas;
     }
 
     public boolean isInscripcion() {
--- PG/ESPORTS/src/Modelo/Jornada.java
+++ PG/ESPORTS/src/Modelo/Jornada.java
@@ -1,8 +1,44 @@
 package Modelo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Jornada {
     private int numero;
     private LocalDate fecha;
+    private ArrayList<Jornada> listaJornadas;
+
+    public Jornada() {
+
+    }
+
+    public Jornada(int numero, LocalDate fecha, ArrayList<Jornada> listaJornadas) {
+        this.numero = numero;
+        this.fecha = fecha;
+        this.listaJornadas = listaJornadas;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public ArrayList<Jornada> getListaJornadas() {
+        return listaJornadas;
+    }
+
+    public void setListaJornadas(ArrayList<Jornada> listaJornadas) {
+        this.listaJornadas = listaJornadas;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
 }
