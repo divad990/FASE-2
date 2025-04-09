52454e0b95792dee2ccef180b285c56d3a40600e
ikeerhg <80524650+ikeerhg@users.noreply.github.com>
2025-04-09 12:15:41 +0200

    PROGRAMACION ESTRUCTURA MVC Y JPA
    ESTRUCTURA DE NUESTRO PROGRAMA DE ESPORTS ( MVC Y JPA )

---
+++ PG/ESPORTS FASE 3/Esports-fase3/src/main/java/Controller/ModeloController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+public class ModeloController {
+
+    private VistaController vistaController;
+    public ModeloController() {
+
+    }
+
+    public VistaController getVistaController() {
+        return vistaController;
+    }
+
+    public void setVistaController(VistaController vistaController) {
+        this.vistaController = vistaController;
+    }
+}
---
+++ PG/ESPORTS FASE 3/Esports-fase3/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import javax.naming.ldap.Control;
+
+public class Main {
+
+    private static Controller.ModeloController modeloController;
+    private static Controller.VistaController vistaController;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void crearObjetos()
+    {
+        // Modelo
+        modeloController = new Controller.ModeloController();
+        vistaController = new Controller.VistaController(modeloController);
+        modeloController.setVistaController(vistaController);
+
+    }
+
+}
