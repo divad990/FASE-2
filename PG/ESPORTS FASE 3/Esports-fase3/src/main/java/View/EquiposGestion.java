8d4fe82da2e029b7361af4788503917a220cbe1f
gorkaemaldi <gorka.emaldi@ikasle.egibide.org>
2025-04-11 10:03:27 +0200

    ultimos cambios GestionEquipos

---
+++ PG/ESPORTS FASE 3/Esports-fase3/src/main/java/View/EquiposGestion.java
@@ -0,0 +1,17 @@
+package View;
+
+import javax.swing.*;
+
+public class EquiposGestion {
+    private JPanel pPrincipal;
+    private JComboBox comboBox1;
+    private JPanel pDatos;
+    private JPanel pNombre;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPanel pFecha;
+    private JPanel pJugadores;
+    private JTextField textField3;
+    private JButton button1;
+    private JButton button2;
+}
